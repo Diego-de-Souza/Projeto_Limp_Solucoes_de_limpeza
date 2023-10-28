d0c8145bf0793d46bcdaea988dab7d683ef4e838
Felipe <felipepradoferrera@hotmail.com>
2023-10-27 21:21:27 -0300

    Aplicando slideshow no index

--- scripts/index-scripts.js
+++ scripts/index-scripts.js
@@ -14,6 +14,8 @@ for(let i = 0; i < exampleServices.length; i++){
     //creating elements
     const service = document.createElement("service")
     //manipulating elements
+
+    /*
     service.innerHTML=
     `
     <div class="services">
@@ -26,6 +28,7 @@ for(let i = 0; i < exampleServices.length; i++){
     //adding elements to the DOM
     let services_box_elements = document.querySelector(".flex-box-services")
     services_box_elements.appendChild(service)
+    */
 }
 
 //Deposition boxes
@@ -61,4 +64,35 @@ for(let i = 0; i < clientDepositions.length; i++){
     //adding elements to the DOM
     const deposition_box_elements = document.querySelector(".flex-box-depositions")
     deposition_box_elements.appendChild(deposition)
+}
+
+/*############# Slideshow content #############*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
