0411c3f5c6322303827d0da4aab1dd0bea8a425c
Felipe <felipepradoferrera@hotmail.com>
2023-11-12 03:15:37 -0300

    Testes com menu hamburger

--- scripts/test-scripts.js
+++ scripts/test-scripts.js
@@ -1,34 +1,13 @@
-/*############# Depositions #############*/
-const clientDepositions=[{
-    photo: "",
-    title: 'Cliente 1',
-    depo: 'Deposição 1 ... ... ... ... ... ... ... ... ... ...'
-},{
-    photo: "",
-    title: 'Cliente 2',
-    depo: 'Deposição 2 ... ... ... ... ... ... ... ... ... ...'
-},{
-    photo: "",
-    title: 'Cliente 3',
-    depo: 'Deposição 3 ... ... ... ... ... ... ... ... ... ...'
-}]
-for(let i = 0; i < clientDepositions.length; i++){
-    //creating elements
-    const deposition = document.createElement("deposition")
-    //manipulating elements
-    deposition.innerHTML=
-    `
-    <div class="depositions">
-    <img class="generic-client-img" src="./img/index-images/generic-client.png" alt="generic client">
-        <h1>${clientDepositions[i].title}</h1>
-        <div class="depositions-content">
-            <p>
-                ${clientDepositions[i].depo}
-            </p>
-        </div>
-    </div>
-    `
-    //adding elements to the DOM
-    const deposition_box_elements = document.querySelector(".flex-box-depositions")
-    deposition_box_elements.appendChild(deposition)
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault()
+
+    const nav = document.getElementById('nav-menu')
+    const hamburger = document.getElementById('hamburger')
+    nav.classList.toggle('active')
+    hamburger.classList.toggle('active')
 }
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
